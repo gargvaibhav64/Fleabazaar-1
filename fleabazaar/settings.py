1f103018486d1f3569888cf9ceaa5c39e4261b98
Vaibhav <gargvaibhav64@gmail.com>
2020-01-14 22:18:48 +0530

    Added settings for heroku

--- fleabazaar/settings.py
+++ fleabazaar/settings.py
@@ -1,3 +1,4 @@
+import dj_database_url
 import os
 
 ENVIRONMENT = os.getenv('ENVIRONMENT', 'development')
@@ -106,3 +107,12 @@ ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 MERCHANT_ID = 'yGhiFc56070841982837'
 MERCHANT_KEY = 'Be7uTRD@7R9QOGN3'
+
+# Heroku: Update database configuration from $DATABASE_URL.
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
